5dd311aef1a6ea3ff16e083dcc2d9864920a5ce8
Dung-Ru Tsai <octobersky.tw@gmail.com>
2022-08-07 21:37:58 +0800

    first

---
+++ 2022/kick_start/centauri_prime.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string GetRuler(const string& kingdom) {
+    // TODO: implement this method to determine the ruler name, given the kingdom.
+    string ruler = "Bob";
+    int len = kingdom.length();
+    char c = kingdom[len - 1];
+    c = c | 0x20; //to lower case
+    if(c == 'a' ||
+    c == 'e' ||
+    c == 'i' ||
+    c == 'o'||
+    c == 'u') {
+      ruler = "Alice";
+    } else if(c == 'y') {
+      ruler = "nobody";
+    }
+    return ruler;
+}
+
+int main() {
+  int testcases;
+  cin >> testcases;
+  string kingdom;
+
+  for (int t = 1; t <= testcases; ++t) {
+    cin >> kingdom;
+    cout << "Case #" << t << ": " << kingdom << " is ruled by "
+         << GetRuler(kingdom) << ".\n";
+  }
+  return 0;
+}
