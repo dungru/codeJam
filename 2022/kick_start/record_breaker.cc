113e8bc8ca4e51552630f8b331bd9627023270f0
Dung-Ru Tsai <octobersky.tw@gmail.com>
2022-08-29 17:12:08 +0800

    Fix coding style

--- 2022/kick_start/record_breaker.cc
+++ 2022/kick_start/record_breaker.cc
@@ -8,22 +8,23 @@ int NumberOfRecordBreakingDays(int numberOfDays, vector<int> visitors)
 {
     // TODO: Complete this function to get the number of record breaking days
     stack<int> stk;
-    if(numberOfDays == 1)
+    if (numberOfDays == 1)
         return 1;
     int ans = 0;
-    if(visitors[0] > visitors[1])
+    if (visitors[0] > visitors[1])
         ans++;
     stk.push(visitors[0]);
 
-    for(int i = 1; i < numberOfDays; i++) {
-        if(i + 1 < numberOfDays && visitors[i] > stk.top() && visitors[i] > visitors[i+1]) {
+    for (int i = 1; i < numberOfDays; i++) {
+        if (i + 1 < numberOfDays && visitors[i] > stk.top() &&
+            visitors[i] > visitors[i + 1]) {
             ans++;
             stk.push(visitors[i]);
-        } else if(i == numberOfDays - 1 && visitors[i] > stk.top()) {
+        } else if (i == numberOfDays - 1 && visitors[i] > stk.top()) {
             ans++;
         }
     }
-    
+
     return ans;
 }
 
