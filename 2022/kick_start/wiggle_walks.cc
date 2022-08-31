ecb7eb2b07f10b5a62b00066e381d8b9b2aff942
Dung-Ru Tsai <octobersky.tw@gmail.com>
2022-08-31 16:22:26 +0800

    Update wiggle walks

--- 2022/kick_start/wiggle_walks.cc
+++ 2022/kick_start/wiggle_walks.cc
@@ -7,51 +7,37 @@
 #include <vector>
 using namespace std;
 
+unordered_map<char, vector<int> > dir;
+
 struct Coordinate {
     int r;
     int c;
     Coordinate(int r, int c) : r(r), c(c) {}
-
-    bool operator==(const Coordinate &coord) const
-    {
-        return (this->r == coord.r && this->c == coord.c);
-    }
-
-    struct HashFunction {
-        size_t operator()(const Coordinate &coord) const
-        {
-            size_t r_hash = std::hash<int>()(coord.r);
-            size_t c_hash = std::hash<int>()(coord.c);
-            return r_hash ^ c_hash;
-        }
-    };
 };
-
-unordered_map<char, vector<int> > dir;
-
 Coordinate EndPosition(int N, int R, int C, int Sr, int Sc, string instructions)
 {
-    Coordinate cur = Coordinate(Sr, Sc);
-    unordered_map <Coordinate, bool, Coordinate::HashFunction> visited;
+    unordered_set <int> us[R + 1];
     dir['N'] = {-1, 0};
     dir['E'] = {0, 1};
     dir['S'] = {1, 0};
     dir['W'] = {0, -1};
-
-    visited[cur] = true;
+    int cur_x = Sr;
+    int cur_y = Sc;
+    us[cur_x].insert(cur_y);
     for(auto i : instructions) {
         vector<int> delta = dir[i];
-        cur.r = cur.r + delta[0];
-        cur.c = cur.c + delta[1];
-        if (cur.r <= 0 || cur.r > R || cur.c <= 0 || cur.c > C)
+        cur_x = cur_x + delta[0];
+        cur_y = cur_y + delta[1];
+        if (cur_x <= 0 || cur_x > R || cur_y <= 0 || cur_y > C)
             continue;
-        while(visited[cur]) {
-            cur.r = cur.r + delta[0];
-            cur.c = cur.c + delta[1];
+        while (us[cur_x].find(cur_y) != us[cur_x].end()) {
+                cur_x += delta[0];
+                cur_y += delta[1];
         }
-        visited[cur] = true;
+        us[cur_x].insert(cur_y);
+
     }
-    return cur;
+    return Coordinate(cur_x, cur_y);
 }
 
 int main()
